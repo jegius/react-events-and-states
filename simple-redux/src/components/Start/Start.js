7c6b19dd9ad830ecfbf52e45395b6e6afb3e623f
Ivan Alekseev <iv.s.alekseev@tinkoff.ru>
2023-08-16 14:08:21 +0300

    Исправен бесконечный цикл в useEffect

--- simple-redux/src/components/Start/Start.js
+++ simple-redux/src/components/Start/Start.js
@@ -1,28 +1,29 @@
 import './Start.css';
-import React, {useEffect, useState} from 'react';
+import React, {useEffect} from 'react';
 import {useDispatch, useSelector} from 'react-redux';
 import {selectIsStarted} from '../../store/selectors';
 import {setItems, setStart} from '../../store/actions';
 
 export function Start() {
     const isStarted = useSelector(selectIsStarted);
-    const [poolingInterval, setPoolingInterval] = useState();
     const dispatch = useDispatch();
 
     useEffect(() => {
-        clearInterval(poolingInterval);
+        let newInterval;
         if (isStarted) {
-            const newInterval = setInterval(async () => {
+            newInterval = setInterval(async () => {
                 const response = await fetch('http://localhost:3001/api/voteItems');
                 const result = await response.json();
                 dispatch(setItems(result));
-            }, 200)
-            setPoolingInterval(newInterval);
-        } else {
-            clearInterval(poolingInterval);
+            }, 500)
         }
-    }, [dispatch, isStarted, poolingInterval])
 
+        return () => {
+            if (newInterval) {
+                clearInterval(newInterval);
+            }
+        }
+    }, [dispatch, isStarted])
 
     const handleSubmit = async () => {
         const partOfPath = isStarted ? 'stop' : 'start';
@@ -36,6 +37,8 @@ export function Start() {
     return (
         <button
             onClick={handleSubmit}
-            className={`start-button ${isStarted ? 'stop' : 'start'}`}>{isStarted ? 'stop' : 'start'}</button>
+            className={`start-button ${isStarted ? 'stop' : 'start'}`}>
+            {isStarted ? 'stop' : 'start'}
+        </button>
     );
 }
